44e794b7ad85eedba36ad0d39600cff519eae78a
Andrew  S. Messecar <38142726+AndrewMessecar@users.noreply.github.com>
2023-12-09 22:20:23 -0500

    Uploaded original files
    Initial commit of original README file and Lua script.

---
+++ Fityk_MoS2_Raman_Spectra_Batch_Analysis.lua
@@ -0,0 +1,27 @@
+function Import_and_Fit_Spectra_From_Folder( Folder_Directory , File_Path )
+
+   local p = io.popen('find "' .. Folder_Directory .. '" -type f')  --Open directory look for files, save data in p. By giving '-type f' as parameter, it returns all files.     
+   for file in p:lines() do                         --Loop through all files
+      -- print(file)
+      F:execute("@0 < " .. file) -- Imports a single spectrum into Fityk.
+      F:execute("@0: info title >> " .. File_Path) -- Writes the name of the file to the output Tab-Separated Value (TSV) file.
+      F:execute("@0: guess Linear; fit") -- Initializes and fits a linear background to the imported spectrum.
+      F:execute("@0: A = x > 370 and x < 430") -- Designates the active area for peak fitting to be between 370 and 430 inverse centimeters (cm^-1)
+      F:execute("@0: delete(not a)") -- Deletes the inactive area of the spectrum where peaks are not being fitted.
+      F:execute("@0: guess Lorentzian") -- Initializes three Lorentzian profiles within the active area.
+      F:execute("@0: guess Lorentzian")
+      F:execute("@0: guess Lorentzian")
+      F:execute("@0: fit") -- Fits the three Lorentzian profiles and linear background to the active area.
+      F:execute("@0: info peaks >> " .. File_Path) -- Writes parameters of the fitted linear background and three Lorentzian profiles to the output.
+      F:execute("delete @0") -- Deletes the spectrum and its fitted features before reiterating the loop and loading the next spectrum.
+   end
+end
+
+Folder_Path = --File path to local directory where the spectra .txt files of interest are stored.
+
+Output_File = --Complete file path including the name of the Tab-Separated Value (.tsv) file where the output of the Fityk batch processing will be written.
+
+Import_and_Fit_Spectra_From_Folder( Folder_Path , Output_File )
+
+print("Batch Processing Is Now Complete")
+
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Batch_Peak_Fitting_Lua_Fityk
-Lua script for the batch fitting of peaks in spectral data via the Fityk open-source software tool.
+Andrew S. Messecar, 2023
+
+This Lua script was developed to facilitate the batch processing of large amounts of Raman spectra acquired of molybdenum disulfide (MoS2) samples through the Fityk open-source peak fitting software. The script does not require the installation of LuaRocks or any additional Lua packages, but it does require both Lua 5 and Fityk to be locally installed.
+
+Before the script can be used, it needs to be edited to designate (1) the local directory in which the .txt spectra files to be processed are currently located and (2) the file path (including the name) of the Tab-Separated Value (TSV) file where the user desires the output of the fitting process to be written.
+
+With the above two modifications made, the script can be used by opening a terminal, changing the directory to the folder where the script is currently saved, and executing the command "cfityk Fityk_MoS2_Raman_Spectra_Batch_Analysis.lua". The script will execute via Fityk in "headless" mode in the terminal window.
+
+The program currently initializes and then fits a linear background as well as three (3) Lorentzian profiles to the portion of the Raman spectra located between 370 and 430 inverse centimeters. By modifying the script, this specified active area can be altered or removed entirely. Additionally, the number and nature of the peak profiles to be fit to the data can be changed. These modifications can allow this script to be used to batch process spectral data characteristic of additional systems of interest to the user.
+
+Reference
+
+Wojdyr, M. (2010). Fityk: a general-purpose peak fitting program. J. Appl. Cryst. 43, 1126-1128. https://doi.org/10.1107/S0021889810030499
